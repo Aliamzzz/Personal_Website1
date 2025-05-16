b7f890d81e9f21c415ef0ec6fba5442b05770880
Aliamzzz <ajangali2000@gmail.com>
2025-05-16 20:01:50 +0330

    Completed My projects
    Completed MyProjects section

--- MyProjects/models.py
+++ MyProjects/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, blank=False, null=False)
+    shortDescription = models.CharField(max_length=100, blank=True, null=True)
+    fullDescription = models.TextField(max_length=300, blank=True, null=True)
     image = models.ImageField(upload_to='projects_images/')
-    link = models.CharField(max_length=100)
+    link = models.CharField(max_length=100, blank=True, null=True)
+    period = models.CharField(max_length=100, blank=True, null=True)
+    tools = models.CharField(max_length=200, blank=True, null=True)
+
 
 
     def __str__(self):
