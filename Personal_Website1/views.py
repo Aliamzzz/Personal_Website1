2413f3fcf252b0a498d79609024c857c4828651d
Aliamzzz <ajangali2000@gmail.com>
2025-07-05 06:01:00 +0330

    contact me
    + fixed contact me section
    + added site title
    + fixed home bug in navbar
    + fixed get method in contact me section
    - removed header logo
    - removed features section

--- Personal_Website1/views.py
+++ Personal_Website1/views.py
@@ -10,4 +10,14 @@ def home(request):
     Jobs = Job.objects.all()
     ContactMe = Contact.objects.all()
     MyProjects = Project.objects.all()
-    return render(request, 'index.html', context={'Talents': Talents, 'Jobs': Jobs, 'ContactMe': ContactMe, 'MyProjects': MyProjects})
+    return render(request, 'index.html', context={'Talents': Talents, 'Jobs': Jobs,
+                                                  'ContactMe': ContactMe, 'MyProjects': MyProjects})
+
+def contact(request):
+    name = request.GET.get('name')
+    family = request.GET.get('family')
+    email = request.GET.get('email')
+    phone = request.GET.get('phone')
+    message = request.GET.get('message')
+    return render(request, 'index.html', context={'name': name, 'family': family,
+                                                  'email': email, 'phone': phone, 'message': message})
